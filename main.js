1298edab0ca6f6413ef0cb3245192a9dbe82d344
Oscar Brunelle <oscar.brunelle@gmail.com>
2022-11-07 00:14:08 +0100

    Added random selection of songs

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 let count_duration = 10;
 let count = count_duration;
 let count_start_time = null;
+const songs_path = "songs/";
+let songs_category = "unknown";
+let songs_in_category = [];
 
-let current_audio = null;
+let current_audio_player = null;
+let current_song_info = null;
 
 function load() {
 	document.querySelector("#counter").style.display = "none";
@@ -13,21 +17,27 @@ function start_blindtest() {
 	document.querySelector("#start_blindtest-button").remove();
 	document.querySelector("#counter").style.display = "grid";
 	document.querySelector("#song-title").style.display = "grid";
+	set_songs_in_category();
+	if (songs_in_category.length <= 0) {
+		console.error("No songs in playlist");
+		return;
+	}
 	play_new_random_music();
 }
 
 function choose_random_song() {
-	return "songs/Gunther - Ding Dong Song.mp3";
+	const random_song_index = random_int(0, songs_in_category.length - 1);
+	return songs_in_category[random_song_index];
 }
 
 function play_new_random_music() {
-	if (current_audio != null) {
-		current_audio.pause();
+	if (current_audio_player != null) {
+		current_audio_player.pause();
 	}
-	let song_path = choose_random_song();
-	current_audio = new Audio(song_path);
-	current_audio.volume = 0.1;
-	current_audio.play();
+	current_song_info = choose_random_song();
+	current_audio_player = new Audio(songs_path + current_song_info.file_name);
+	current_audio_player.volume = 0.1;
+	current_audio_player.play();
 	start_countdown();
 }
 
@@ -43,9 +53,19 @@ function update_countdown() {
 	document.querySelector("#counter>span").innerHTML = displayed_count;
 	if (displayed_count == 0) {
 		clearInterval(update_countdown_interval);
-		document.querySelector("#counter>span").innerHTML = "my tralala";
+		document.querySelector("#counter>span").innerHTML = current_song_info.title;
 		setTimeout(play_new_random_music, 5000);
 	}
+}
+
+function set_songs_in_category() {
+	songs_in_category = [];
+	for (song of SONGS_LIST) {
+		if (song.categories.indexOf(songs_category) >= 0) {
+			songs_in_category.push(song);
+		}
+	}
+	return songs_in_category;
 }
 
 document.onload = load();
---
+++ songs_list.js
@@ -0,0 +1,21 @@
+const SONGS_LIST = [{
+	"title": "Never Gonna Give You Up",
+	"artist": "Rick Asley",
+	"categories": ["internet", "unknown"],
+	"file_name": "Rick Astley - Never Gonna Give You Up (Official Music Video).mp3",
+}, {
+	"title": "Ding Dong Song",
+	"artist": "Gunther",
+	"categories": ["unknown"],
+	"file_name": "Gunther - Ding Dong Song.mp3",
+}, {
+	"title": "Tommy's Theme",
+	"artist": "Noisia",
+	"categories": ["unknown"],
+	"file_name": "Noisia - Tommy's Theme (Official Video).mp3",
+// }, {
+// 	"title": "Never",
+// 	"artist": "Rick",
+// 	"categories": ["unknown"],
+	"file_name": "",
+}];
